9ced8173b2e86cec239395df52aef1d2621ebcb4
Samuel Kinyanjui <sax80kinya@gmail.com>
2023-10-20 23:49:36 +0300

    updated
    fetched data via api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './index.css';
 import { Calculates } from './components/Calculator';
+import Quote from './Quotes/Quote';
 
 function App() {
   return (
-
-    <Calculates />
+    <>
+      <div className="main">
+        <Calculates />
+        <Quote />
+      </div>
+    </>
 
   );
 }
